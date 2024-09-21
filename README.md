9cf76401ea9b52a0f296e41b8fc0687419eefcb7
Terraformer9x <83795041+Terraformer9x@users.noreply.github.com>
2024-09-21 04:31:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## This mod is WIP!
+
+# Rebalanced Mineshaft
+Rebalances the Mineshaft interior to be more akin to how Factory and Mansion are balanced. It also disables Mineshaft spawning 6 extra loot and Maneaters having increased spawn chance on them, these will be made configurable at later point.
+
+# Contact
+If you have any questions or have any reports to make, you can add me on Discord: @Terraformer9x
---
+++ RebalancedMineshaft/Plugin.cs
@@ -0,0 +1,233 @@
+﻿using BepInEx;
+using DunGen;
+using DunGen.Graph;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace RebalancedMineshaft;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+    public static Plugin Instance;
+
+    private void Awake()
+    {
+        Instance ??= this;
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        harmony.PatchAll(typeof(MineshaftPatch));
+    }
+
+    public static void Log(string msg) => Instance.Logger.LogInfo(msg);
+    public static void LogDebug(string msg) => Instance.Logger.LogDebug(msg);
+    public static void LogWarning(string msg) => Instance.Logger.LogWarning(msg);
+}
+
+public class MineshaftPatch
+{
+    [HarmonyPatch(typeof(Dungeon), "PreGenerateDungeon")]
+    [HarmonyPostfix]
+    private static void PreGenerateDungeonPostfix(Dungeon __instance)
+    {
+        if (__instance.DungeonFlow.name == "Level3Flow")
+        {
+            Plugin.LogDebug(__instance.DungeonFlow.name + $" detected, changing generation size.");
+            __instance.DungeonFlow.Length.Min = 11;
+            __instance.DungeonFlow.Length.Max = 14;
+            __instance.DungeonFlow.Lines[0].Length = 0.35f;
+            __instance.DungeonFlow.Lines[1].Position = 0.35f;
+            __instance.DungeonFlow.Lines[1].Length = 0.3f;
+            __instance.DungeonFlow.Lines[2].Position = 0.65f;
+            __instance.DungeonFlow.Lines[2].Length = 0.35f;
+            __instance.DungeonFlow.Lines[0].DungeonArchetypes[0].BranchCount.Min = 9;
+            __instance.DungeonFlow.Lines[0].DungeonArchetypes[0].BranchCount.Max = 13;
+            __instance.DungeonFlow.Lines[1].DungeonArchetypes[0].BranchCount.Min = 4;
+            __instance.DungeonFlow.Lines[1].DungeonArchetypes[0].BranchCount.Max = 6;
+            //__instance.DungeonFlow.Lines[2].DungeonArchetypes[0].BranchCount.Min = 9;
+            //__instance.DungeonFlow.Lines[2].DungeonArchetypes[0].BranchCount.Max = 13;
+        }
+    }
+
+    [HarmonyPatch(typeof(Dungeon), "PostGenerateDungeon")]
+    [HarmonyPostfix]
+    private static void PostGenerateDungeonPostfix(Dungeon __instance)
+    {
+        if (__instance.DungeonFlow.name != "Level3Flow") return;
+
+        foreach (Tile tile in __instance.AllTiles)
+        {
+            switch (tile.name)
+            {
+                case "CaveSmallIntersectTile(Clone)":
+                    Transform tempTransform = tile.transform.Find("GeneralScrapSpawn")?.transform;
+                    if (tempTransform != null)
+                    {
+                        Plugin.LogDebug($"Converting to random spawn at " + tempTransform.position.x + ", " + tempTransform.position.y + ", " + tempTransform.position.z);
+                        Functions.ConvertToRandomSpawn(tempTransform, 0.15f, 0.25f);
+                    }
+                    break;
+
+                case "CaveCrampedIntersectTile(Clone)":
+                    tempTransform = tile.transform.Find("GeneralScrapSpawn")?.transform;
+                    if (tempTransform != null)
+                    {
+                        Plugin.LogDebug($"Converting to random spawn at " + tempTransform.position.x + ", " + tempTransform.position.y + ", " + tempTransform.position.z);
+                        Functions.ConvertToRandomSpawn(tempTransform, 0.15f, 0.25f);
+                    }
+                    break;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DungeonGenerator), "ProcessGlobalProps")]
+    [HarmonyPostfix]
+    private static void ProcessGlobalPropsPostfix(DungeonGenerator __instance)
+    {
+        if (__instance.DungeonFlow.name != "Level3Flow") return;
+
+        foreach (Tile tile in __instance.CurrentDungeon.AllTiles)
+        {
+            switch (tile.name)
+            {
+                case "TunnelSplit(Clone)":
+                    Transform tempTransform = tile.transform.Find("SouthWallProps/Shelf1 (14)")?.transform;
+                    if (tempTransform != null)
+                    {
+                        Plugin.LogDebug($"Found a shelf at " + tempTransform.position.x + ", " + tempTransform.position.y + ", " + tempTransform.position.z);
+                        Functions.CreateItemSpawn(tempTransform, new Vector3(-7.67999983f, -6.5999999f, 1.78999996f), "SmallItems", 15, true);
+                    }
+
+                    tempTransform = tile.transform.Find("Props/PropSet2/WoodPalletPile2x")?.transform;
+                    if (tempTransform != null)
+                    {
+                        Plugin.LogDebug($"Found a pallet at " + tempTransform.position.x + ", " + tempTransform.position.y + ", " + tempTransform.position.z);
+                        Functions.CreateItemSpawn(tempTransform, new Vector3(0.300000012f, 0.0399999991f, 2.53999996f), "TabletopItems", 3, false);
+                    }
+
+                    tempTransform = tile.transform.Find("Props/PropSet2/Minecart (1)")?.transform;
+                    if (tempTransform != null)
+                    {
+                        Plugin.LogDebug($"Found a minecart at " + tempTransform.position.x + ", " + tempTransform.position.y + ", " + tempTransform.position.z);
+                        Transform itemSpawn = Functions.CreateItemSpawn(tempTransform, new Vector3(-0.74000001f, 1.22000003f, 4.11000013f), "SmallItems", 1, false)?.transform;
+                        if (itemSpawn != null) Functions.ConvertToRandomSpawn(itemSpawn, 0.25f, 0.8f);
+                    }
+                    break;
+
+                case "TunnelSplitEndTile(Clone)":
+                    tempTransform = tile.transform.Find("SouthWallProps/Shelf1 (14)")?.transform;
+                    if (tempTransform != null)
+                    {
+                        Plugin.LogDebug($"Found a shelf at " + tempTransform.position.x + ", " + tempTransform.position.y + ", " + tempTransform.position.z);
+                        Functions.CreateItemSpawn(tempTransform, new Vector3(-7.67999983f, -6.5999999f, 1.78999996f), "SmallItems", 15, true);
+                    }
+                    break;
+
+                case "CaveCrampedIntersectTile(Clone)":
+                    GameObject tempObject = tile.transform.Find("TablePropSpawn")?.gameObject;
+                    if (tempObject != null)
+                    {
+                        Plugin.LogDebug($"Destroying table prop spawn at " + tempObject.transform.position.x + ", " + tempObject.transform.position.y + ", " + tempObject.transform.position.z);
+                        GameObject.Destroy(tempObject);
+                    }
+                    break;
+
+                case "CaveWaterTile(Clone)":
+                    tempObject = tile.transform.Find("MapHazardSpawnType1")?.gameObject;
+                    if (tempObject != null)
+                    {
+                        Plugin.LogDebug($"Destroying hazard spawn at " + tempObject.transform.position.x + ", " + tempObject.transform.position.y + ", " + tempObject.transform.position.z);
+                        GameObject.Destroy(tempObject);
+                    }
+                    break;
+            };
+        }
+    }
+
+    private class Functions
+    {
+        internal static GameObject CreateItemSpawn(Transform transform, Vector3 localPosition, string spawn, int range, bool copy)
+        {
+            GameObject obj = new GameObject();
+            obj.name = $"AddedItemSpawn_" + spawn;
+            obj.transform.SetParent(transform);
+            obj.transform.localPosition = localPosition;
+            RandomScrapSpawn randomScrapSpawn = obj.AddComponent<RandomScrapSpawn>();
+            randomScrapSpawn.spawnableItems = GetItemGroupFromString(spawn);
+            randomScrapSpawn.itemSpawnRange = range;
+            randomScrapSpawn.spawnedItemsCopyPosition = copy;
+
+            return obj;
+        }
+
+        internal static GameObject ConvertToRandomSpawn(Transform transform, float mainWeight, float branchWeight)
+        {
+            GameObject obj = new GameObject();
+            obj.name = $"AddedRandomSpawn_" + transform.name;
+            obj.transform.SetParent(transform.parent);
+            transform.SetParent(obj.transform);
+            LocalPropSet localPropSet = obj.AddComponent<LocalPropSet>();
+
+            GameObjectChance objSpawnChance = new GameObjectChance();
+            objSpawnChance.Value = transform.gameObject;
+            objSpawnChance.MainPathWeight = mainWeight;
+            objSpawnChance.BranchPathWeight = branchWeight;
+
+            localPropSet.Props.Weights.Add(objSpawnChance);
+            localPropSet.PropCount.Min = 0;
+            localPropSet.PropCount.Max = 1;
+
+            return obj;
+        }
+
+        internal static ItemGroup GetItemGroupFromString(string str)
+        {
+            foreach (ItemGroup itr in Resources.FindObjectsOfTypeAll<ItemGroup>())
+            {
+                if (itr.name == str) return itr;
+            }
+            return null;
+        }
+    }
+
+    [HarmonyPatch(typeof(StartOfRound), "Awake")]
+    [HarmonyPostfix]
+    private static void ChangeManeaterData(StartOfRound __instance)
+    {
+        foreach (EnemyType enemyType in Resources.FindObjectsOfTypeAll<EnemyType>())
+        {
+            if(enemyType.name == "CaveDweller")
+            {
+                enemyType.increasedChanceInterior = -1;
+                break;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(RoundManager), "SpawnScrapInLevel")]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> ChangeMineshaftAmount(IEnumerable<CodeInstruction> instructions)
+    {
+        CodeMatcher codeMatcher = new CodeMatcher(instructions)
+            .MatchForward(true,
+                new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(RoundManager), "currentDungeonType"))
+            )
+            .MatchForward(false,
+                new CodeMatch(OpCodes.Ldc_I4_6)
+            )
+            .SetInstruction(
+                new CodeInstruction(OpCodes.Ldc_I4_0)
+            );
+
+        return codeMatcher.InstructionEnumeration();
+    }
+}
